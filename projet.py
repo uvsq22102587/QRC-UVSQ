928cde6a4305f4003c64f6df7e2a2cbe1fcb203a
Valentin JACQUIN <valentin.jacquin@ens.uvsq.fr>
2022-04-07 08:33:00 +0200

    Code de hamming updated

--- projet.py
+++ projet.py
@@ -134,20 +134,23 @@ def verifLignesQRC(matrice):
 verifLignesQRC(loading("qr_code_ssfiltre_ascii_rotation.png"))
 
 
+
 def correctionHamming(bits):
     # calcul bits de controle
-    p1 = bits[2] ^ bits[4] ^ bits[6]
-    p2 = bits[2] ^ bits[5] ^ bits[6]
-    p3 = bits[4] ^ bits[5] ^ bits[6]
+    p1 = bits[0] ^ bits[1] ^ bits[2]
+    p2 = bits[0] ^ bits[2] ^ bits[3]
+    p3 = bits[1] ^ bits[2] ^ bits[3]
     # position erreur
     num = int(p1 != bits[0]) + int(p2 != bits[1])*2 + int(p3 != bits[3]) * 4
-    if num in [3, 5, 6, 7]:
-        # on remplace bits[num-1] par 0 s'il valait 1 et par 1 si il valait 0
-        bits[num-1] = int(not bits[num - 1])
-        print("corection d'un pixel corrompu en position " + str(num) + "\n")
-        return [bits[2], bits[4], bits[5], bits[6]]
-    else:
-        return [bits[2], bits[4], bits[5], bits[6]]
+    if (num == 3):
+        bits[0] = int(not bits[0])
+    if num == 5:
+        bits[1] = int(not bits[1])
+    if num == 6:
+        bits[2] = int(not bits[2])
+    if num == 7:
+        bits[3] = int(not bits[3])
+    return bits[:4]
 
 def HammingRead(matrice):
     """Fonction qui prend une matrice de pixels et la lit en effectuant la vérification via le code de Hamming."""
